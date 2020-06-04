2533ebd2f3bcec2efa28a165d6665fc853e80b72
pafau <rkr1410@gmail.com>
2020-06-04 19:20:01 +0200

    Simplify Source
    * Simplifies the Source class.
    * Adds missing Source unit tests
    * Gets rid of IfElseChain. It was fun while it lasted.

--- src/main/java/net/rkr1410/lang/frontend/Source.java
+++ src/main/java/net/rkr1410/lang/frontend/Source.java
@@ -1,16 +1,8 @@
 package net.rkr1410.lang.frontend;
 
-import net.rkr1410.util.IfElseChain;
-import net.rkr1410.util.LearningExperience;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Objects;
-import java.util.concurrent.Callable;
-import java.util.function.Supplier;
-
-import static net.rkr1410.util.RiskType.WRONG_ABSTRACTION_LEVEL;
-import static net.rkr1410.util.Utils.sneakThrow;
 
 /**
  * <h1>Source</h1>
@@ -18,81 +10,89 @@ import static net.rkr1410.util.Utils.sneakThrow;
  * <p>A framework class representing the source program</p>
  */
 public class Source {
-    private static final int BEFORE_START_OF_FILE = -2;
-    private static final int BEFORE_START_OF_LINE = -1;
-
     public static final char EOL = '\n';
     public static final char EOF = (char) 0;
 
     private BufferedReader reader;
     private String line;
     private int lineNumber;
-    private int lineOffset;
+    private int linePosition;
+    private boolean initialized;
 
     /**
      * Constructor
      *
      * @param reader the reader for source program
-     * @throws IOException if an I/O error occurred
      */
-    public Source(BufferedReader reader) throws IOException {
+    public Source(BufferedReader reader) {
         this.reader = Objects.requireNonNull(reader, "Reader cannot be null");
         this.lineNumber = 0;
-        this.lineOffset = BEFORE_START_OF_FILE;
     }
 
     /**
-     * Returns character from source at current position.
-     * <p>
-     * This will move the read index to starting character if reading has not started yet.
-     *
-     * @return source char at current position
-     * @throws Exception if an error occurs
-     */
-    public char currentChar() throws Exception {
-        return new IfElseChain<Character>()
-                .ifThen(this::outsideLineBounds, sneakily(this::readLineAndNextCharacter))
-                .ifThen(this::atEndOfFile, () -> EOF)
-                .ifThen(this::atEndOfLine, () -> EOL)
-                .elseDefault(() -> line.charAt(lineOffset));
-    }
-
-    /**
-     * Returns next character from source. Current one is consumed
-     * (calls to <code>currentChar()</code> will no longer return it,
-     * returning this one instead).
-     * <p>
-     * This will return <code>EOL</code> if called as first method on a fresh reader.
+     * Returns next character from source.
+     * Current one is consumed.
      *
      * @return next source character
-     * @throws Exception if an error occurs
+     * @throws IOException if an I/O error occurred
      */
-    public char nextChar() throws Exception {
-        if (lineOffset == BEFORE_START_OF_FILE) {
-            throw new IllegalStateException("Call currentChar before calling nextChar!");
+    public char nextChar() throws IOException {
+        ensureAtLeastOneLineWasRead();
+        if (sourceLineAvailable()) {
+            if (linePosition < line.length()) {
+                ++linePosition;
+            } else {
+                readLine();
+            }
         }
-        ++lineOffset;
         return currentChar();
     }
 
     /**
-     * Returns next character from source. Current one is <b>not</b> consumed
-     * (calls to <code>currentChar()</code> will still return it)
+     * Returns character from source at current position.
+     *
+     * @return source char at current position
+     * @throws IOException if an I/O error occurred
+     */
+    public char currentChar() throws IOException {
+        ensureAtLeastOneLineWasRead();
+        return charAtOffset(0);
+    }
+
+    /**
+     * Returns next character from source.
+     * Current one is not consumed.
      *
      * @return next source character
-     * @throws Exception if an error occurs
+     * @throws IOException if an I/O error occurred
      */
-    public char peekChar() throws Exception {
-        // TODO copy this to nextChar (instead of throwing if nothing is read yet)
-        if (outsideLineBounds()) {
-            readLineAndNextCharacter();
-        }
-        if (line == null) {
-            return EOF;
-        }
+    public char peekChar() throws IOException {
+        ensureAtLeastOneLineWasRead();
+        return charAtOffset(1);
+    }
 
-        int nextPos = lineOffset + 1;
-        return nextPos < line.length() ? line.charAt(nextPos) : EOL;
+    private char charAtOffset(int offset) {
+        if (!sourceLineAvailable()) {
+            return EOF;
+        } else if (offsetOutsideLineBounds(offset)) {
+            return EOL;
+        } else {
+            return line.charAt(linePosition + offset);
+        }
+    }
+
+    private boolean sourceLineAvailable() {
+        return line != null;
+    }
+
+    private boolean offsetOutsideLineBounds(int offset) {
+        return linePosition + offset >= line.length();
+    }
+
+    private void ensureAtLeastOneLineWasRead() throws IOException {
+        if (!initialized) {
+            readLine();
+        }
     }
 
     /**
@@ -102,19 +102,20 @@ public class Source {
      */
     public void readLine() throws IOException {
         line = reader.readLine();
-        lineOffset = BEFORE_START_OF_LINE;
+        linePosition = 0;
 
         if (line != null) {
-            ++lineNumber;
+            lineNumber++;
         }
+        initialized = true;
     }
 
     /**
      * Closes source
      *
-     * @throws Exception if an error occurs
+     * @throws IOException if an I/O error occurred
      */
-    public void close() throws Exception {
+    public void close() throws IOException {
         if (reader != null) {
             try {
                 reader.close();
@@ -125,41 +126,4 @@ public class Source {
         }
     }
 
-    private char readLineAndNextCharacter() throws Exception {
-        readLine();
-        return nextChar();
-    }
-
-    private boolean atEndOfFile() {
-        return line == null;
-    }
-
-    private boolean atEndOfLine() {
-        return lineOffset == BEFORE_START_OF_LINE || lineOffset == line.length();
-    }
-
-    private boolean outsideLineBounds() {
-        return haveNotStartedReadingYet() || movedBeyondCurrentLineEnd();
-    }
-
-    private boolean haveNotStartedReadingYet() {
-        return lineOffset == BEFORE_START_OF_FILE;
-    }
-
-    private boolean movedBeyondCurrentLineEnd() {
-        return line != null && lineOffset > line.length();
-    }
-
-    @LearningExperience(type = WRONG_ABSTRACTION_LEVEL)
-    private static <V> Supplier<V> sneakily(Callable<V> s) {
-        return () -> {
-            try {
-                return s.call();
-            } catch (Exception e) {
-                sneakThrow(e);
-            }
-            // this should never happen as by now we have either returned callable's result or threw
-            return (V) null;
-        };
-    }
 }
--- src/test/java/net/rkr1410/lang/frontend/SourceTest.java
+++ src/test/java/net/rkr1410/lang/frontend/SourceTest.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 
@@ -19,16 +18,19 @@ class SourceTest {
 
     /*
     positions:    0 1 2 3  4 5  6
-    default data: a b c \n w \n \000
+    default data: T e s \n t \n \000
      */
-    private static final String[] DEFAULT_DATA = new String[]{"abc", "w", null};
+    private static final String[] DEFAULT_DATA = new String[]{"Tes", "t", null};
+    private static final char EOL = '\n';
+    private static final char EOF = (char) 0;
 
     @Mock BufferedReader reader;
     Source source;
 
     @Test
     void readerCannotBeNull() {
-        assertThatThrownBy(() -> new Source(null)).isInstanceOf(NullPointerException.class);
+        assertThatThrownBy(() -> new Source(null))
+                .isInstanceOf(NullPointerException.class);
     }
 
 /*
@@ -40,111 +42,217 @@ class SourceTest {
 
     @Test
     void nullDataAndNothingReadYet_currentChar_EOFIsReturned() throws Exception {
-        initSource(null);
+        doReturn(null).when(reader).readLine();
+        initSource();
 
         char c = source.currentChar();
-        assertThat(c).isEqualTo((char) 0);
+        assertThat(c).isEqualTo(EOF);
     }
 
     @Test
     void emptyDataAndNothingReadYet_currentChar_EOLIsReturned() throws Exception {
-        initSource("");
+        doReturn("").when(reader).readLine();
+        initSource();
 
         char c = source.currentChar();
-        assertThat(c).isEqualTo('\n');
+        assertThat(c).isEqualTo(EOL);
     }
 
     @Test
     void someDataAndNothingReadYet_currentChar_firstCharIsReturned() throws Exception {
+        mockDefaultDataReader();
         initSource();
 
         char c = source.currentChar();
-        assertThat(c).isEqualTo('a');
+        assertThat(c).isEqualTo(DEFAULT_DATA[0].charAt(0));
     }
 
     @Test
     void someDataAndSingleCharRead_currentChar_secondCharIsReturned() throws Exception {
+        mockDefaultDataReader();
         initSource();
 
-        source.currentChar();
         source.nextChar();
         char c = source.currentChar();
-        assertThat(c).isEqualTo('b');
+        assertThat(c).isEqualTo(DEFAULT_DATA[0].charAt(1));
     }
 
     @Test
     void someData_afterLineIsRead_currentChar_EOLIsReturned() throws Exception {
+        mockDefaultDataReader();
         initSource();
 
-        source.currentChar();
-        fastForward(3);
+        fastForwardSingleCharacterPastFirstLine();
         char c = source.currentChar();
-        assertThat(c).isEqualTo('\n');
+        assertThat(c).isEqualTo(EOL);
     }
 
     @Test
     void someData_afterAllDataRead_currentChar_EOFIsReturned() throws Exception {
+        mockDefaultDataReader();
         initSource();
 
-        source.currentChar();
-        fastForward(6);
+        fastForwardPastReaderData();
         char c = source.currentChar();
-        assertThat(c).isEqualTo((char) 0);
-    }
-
-    @Test
-    void readerThrows_currentCharThrows() throws IOException {
-        initSource(false, null);
-        doThrow(IOException.class).when(reader).readLine();
-
-        assertThatThrownBy(() -> source.currentChar()).isInstanceOf(IOException.class);
+        assertThat(c).isEqualTo(EOF);
     }
 
 /*
-                               ┌┐┌┌─┐─┐ ┬┌┬┐┌─┐┬ ┬┌─┐┬─┐
-                               │││├┤ ┌┴┬┘ │ │  ├─┤├─┤├┬┘
-                               ┘└┘└─┘┴ └─ ┴ └─┘┴ ┴┴ ┴┴└─
-                      A set of tests verifying nextChar() method results
+                               ┌─┐┌─┐┌─┐┬┌─┌─┐┬ ┬┌─┐┬─┐
+                               ├─┘├┤ ├┤ ├┴┐│  ├─┤├─┤├┬┘
+                               ┴  └─┘└─┘┴ ┴└─┘┴ ┴┴ ┴┴└─
+                   A set of tests verifying peekChar() method results
  */
 
     @Test
-    void nothingReadYet_nextChar_throws() throws Exception {
-        initSource(false, null);
+    void nullDataAndNothingReadYet_peekChar_EOFIsReturned() throws Exception {
+        doReturn(null).when(reader).readLine();
+        initSource();
 
-        assertThatThrownBy(() -> source.nextChar()).isInstanceOf(IllegalStateException.class)
-                .hasMessageContaining("currentChar");
+        char c = source.peekChar();
+        assertThat(c).isEqualTo(EOF);
     }
 
     @Test
-    void someData_afterLineIsReadWithoutNewline_nextChar_EOLIsReturned() throws Exception {
+    void emptyDataAndNothingReadYet_peekChar_EOLIsReturned() throws Exception {
+        doReturn("").when(reader).readLine();
         initSource();
 
-        source.currentChar();
-        fastForward(2);
+        char c = source.peekChar();
+        assertThat(c).isEqualTo(EOL);
+    }
+
+    @Test
+    void someDataAndNothingReadYet_peekChar_secondCharIsReturned() throws Exception {
+        mockDefaultDataReader();
+        initSource();
+
+        char c = source.peekChar();
+        assertThat(c).isEqualTo(DEFAULT_DATA[0].charAt(1));
+    }
+
+    @Test
+    void someDataAndFirstCharConsumed_peekChar_thirdCharIsReturned() throws Exception {
+        mockDefaultDataReader();
+        initSource();
+
+        source.nextChar();
+        char c = source.peekChar();
+        assertThat(c).isEqualTo(DEFAULT_DATA[0].charAt(2));
+    }
+
+    @Test
+    void someData_afterLineIsRead_peekChar_EOLIsReturned() throws Exception {
+        mockDefaultDataReader();
+        initSource();
+
+        fastForwardSingleCharacterPastFirstLine();
+        char c = source.peekChar();
+        assertThat(c).isEqualTo(EOL);
+    }
+
+    @Test
+    void someData_afterAllDataRead_peekChar_EOFIsReturned() throws Exception {
+        mockDefaultDataReader();
+        initSource();
+
+        fastForwardPastReaderData();
+        char c = source.peekChar();
+        assertThat(c).isEqualTo(EOF);
+    }
+
+/*
+                                       ┌┐┌┌─┐─┐ ┬┌┬┐┌─┐┬ ┬┌─┐┬─┐
+                                       │││├┤ ┌┴┬┘ │ │  ├─┤├─┤├┬┘
+                                       ┘└┘└─┘┴ └─ ┴ └─┘┴ ┴┴ ┴┴└─
+                          A set of tests verifying nextChar() method results
+ */
+
+    @Test
+    void someData_afterLineIsReadWithoutNewline_nextChar_EOLIsReturned() throws Exception {
+        mockDefaultDataReader();
+        initSource();
+
+        fastForwardToEndOfFirstLine();
         char c = source.nextChar();
-        assertThat(c).isEqualTo('\n');
+        assertThat(c).isEqualTo(EOL);
     }
 
     @Test
     void someData_afterDataReadIncludingLastNewline_nextChar_EOFKeepsBeingReturned() throws Exception {
+        mockDefaultDataReader();
         initSource();
 
-        source.currentChar();
         fastForward(15);
         char c = source.nextChar();
-        assertThat(c).isEqualTo((char) 0);
+        assertThat(c).isEqualTo(EOF);
     }
 
     @Test
     void readerThrows_nextCharThrows() throws Exception {
         initSource();
-        source.currentChar();
-        fastForward(3);
         doThrow(IOException.class).when(reader).readLine();
 
         assertThatThrownBy(() -> source.nextChar()).isInstanceOf(IOException.class);
     }
 
+/*
+                                      ┌─┐┌─┐┌┬┐┌─┐┬  ┌─┐─┐ ┬
+                                      │  │ ││││├─┘│  ├┤ ┌┴┬┘
+                                      └─┘└─┘┴ ┴┴  ┴─┘└─┘┴ └─
+                    Complex test. Testing curr/peek/nextChar methods at each point
+              of journey through the sample text. This is a sum of other tests, highlighted
+                              as a whole to better visualize the requirements
+ */
+
+    @Test
+    void fullReaderFlowTest() throws Exception {
+        doReturn("BE", "gin", "\n", null).when(reader).readLine();
+        initSource();
+
+        // After first call to either of three methods
+        // current = first character of first line returned by reader
+        verifyStep('B', 'E', 'E');
+        // When curr = last character on the line, next will be returned as EOL
+        // (even though BufferedReader strips newlines)
+        verifyStep('E', EOL, EOL);
+        // Watch out: Source operates on a single line only, peek will return EOL until a call to nextChar
+        verifyStep(EOL, EOL, 'g');
+        verifyStep('g', 'i', 'i');
+        verifyStep('i', 'n', 'n');
+        verifyStep('n', EOL, EOL);
+        verifyStep(EOL, EOL, EOL);
+        verifyStep(EOL, EOL, EOL);
+        // Watch out: Above also applies to finding EOF
+        verifyStep(EOL, EOL, EOF);
+        // After actually getting EOF from nextChar, every call will keep returning EOF until closing
+        verifyStep(EOF, EOF, EOF);
+        verifyStep(EOF, EOF, EOF);
+    }
+
+    private void verifyStep(char expectedCurr, char expectedPeek, char expectedNext) throws Exception {
+        assertThat(source.currentChar()).as("current").isEqualTo(expectedCurr);
+        assertThat(source.peekChar()).as("peek").isEqualTo(expectedPeek);
+        assertThat(source.nextChar()).as("next").isEqualTo(expectedNext);
+    }
+
+    private void fastForwardToEndOfFirstLine() throws Exception {
+        // Already at first char
+        int steps = DEFAULT_DATA[0].length() - 1;
+        fastForward(steps);
+    }
+
+    private void fastForwardSingleCharacterPastFirstLine() throws Exception {
+        // Already at first char
+        int steps = DEFAULT_DATA[0].length();
+        fastForward(steps);
+    }
+
+    private void fastForwardPastReaderData() throws Exception {
+        // +2 for two newlines
+        int steps = DEFAULT_DATA[0].length() + DEFAULT_DATA[1].length() + 2;
+        fastForward(steps);
+    }
 
     private void fastForward(int steps) throws Exception {
         for (int i = 0; i < steps; i++) {
@@ -152,27 +260,11 @@ class SourceTest {
         }
     }
 
+    private void mockDefaultDataReader() throws IOException {
+        doReturn(DEFAULT_DATA[0], DEFAULT_DATA[1], DEFAULT_DATA[2]).when(reader).readLine();
+    }
+
     private void initSource() {
-        initSource(true, null);
+        source = new Source(reader);
     }
-
-    private void initSource(String data) {
-        initSource(false, data);
-    }
-
-    private void initSource(boolean initDefaultData, String data) {
-        try {
-            if (initDefaultData) {
-                doReturn(DEFAULT_DATA[0], DEFAULT_DATA[1], DEFAULT_DATA[2]).when(reader).readLine();
-            } else if (data != null) {
-                doReturn(data).when(reader).readLine();
-            }
-            source = new Source(reader);
-        } catch (IOException e) {
-            fail("Error creating Source");
-            e.printStackTrace();
-        }
-    }
-
-
 }
