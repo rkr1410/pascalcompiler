edb246577e82c04a705d3fafc7caec2559e1f3b3
pafau <rkr1410@gmail.com>
2020-06-04 19:28:35 +0200

    Add Scanner
    Adds abstract Scanner implementation

--- src/main/java/net/rkr1410/lang/frontend/Scanner.java
+++ src/main/java/net/rkr1410/lang/frontend/Scanner.java
@@ -1,11 +1,57 @@
 package net.rkr1410.lang.frontend;
 
-public class Scanner {
-    public Token currentToken() {
-        return null;
-    }
-    public Token nextToken() {
-        return null;
+import java.io.IOException;
+
+/**
+ * <h1>Scanner</h1>
+ *
+ * <p>A language-independent framework class.
+ * To be implemented by language-specific subclasses.</p>
+ */
+public abstract class Scanner {
+
+    protected Source source;
+    private Token currentToken;
+
+    /**
+     * Constructor
+     *
+     * @param source source to use with this Sccaner
+     */
+    public Scanner(Source source) {
+        this.source = source;
     }
 
+    /**
+     * @return current token
+     */
+    public Token currentToken() {
+        return currentToken;
+    }
+
+    /**
+     * @return next token from source
+     * @throws IOException if an I/O error occurs
+     */
+    public Token nextToken() throws IOException {
+        currentToken = extractToken();
+        return currentToken();
+    }
+
+    /**
+     * Extract token from source.
+     * To be implemented by language-specific subclasses.
+     *
+     * @return extracted token
+     * @throws IOException if an I/O error occurs
+     */
+    protected abstract Token extractToken() throws IOException;
+
+    public char currentChar() throws IOException {
+        return source.currentChar();
+    }
+
+    public char nextChar() throws IOException {
+        return source.nextChar();
+    }
 }
--- src/main/java/net/rkr1410/lang/frontend/Token.java
+++ src/main/java/net/rkr1410/lang/frontend/Token.java
@@ -1,4 +1,10 @@
 package net.rkr1410.lang.frontend;
 
+/**
+ * <h1>Token</h1>
+ *
+ * <p>Represents a token returned by {@link Scanner}
+ */
 public class Token {
+
 }
